098f3f2f132052d9a90b01839bd88bb4461b2f2d
MalronWall <thibaut.tourte17@gmail.com>
2019-07-28 19:15:19 +0200

    [UPD] mapping errors fix with trickImage, trickLink and comments / new config mail to test on the server

--- src/UI/Actions/MediasTricksDeleteAction.php
+++ src/UI/Actions/MediasTricksDeleteAction.php
@@ -83,6 +83,7 @@ class MediasTricksDeleteAction implements MediasTricksDeleteActionInterface
                 "Veuillez vous connecter avec d'effectuer cette action !"
             );
         }
-        return $this->responder->response($media->getTrick()->getSlug());
+        $trickMedia = $media->getTypeMedia() == "image" ? $media->getTrickImage() : $media->getTrickLink();
+        return $this->responder->response($trickMedia->getSlug());
     }
 }
